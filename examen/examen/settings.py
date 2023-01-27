fa31d93c1daf522e7b494186fb09be07526d2ebe
Gerardoahf <gerardoahf@gmail.com>
2023-01-27 17:04:42 -0600

    other first start

--- examen/examen/settings.py
+++ examen/examen/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dd8wl&=9k1eowyt3gam7!4h@_#2v3juu-g0yd62su!r9ax+f(w'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
+
 
 ALLOWED_HOSTS = []
 
